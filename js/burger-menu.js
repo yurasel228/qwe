a67768d2e66a2a1bcc5f5eef894ec9a7c48c1e00
yurasel228 <58660807+yurasel228@users.noreply.github.com>
2023-04-26 20:38:58 +0300

    Add files via upload

---
+++ js/burger-menu.js
@@ -0,0 +1,27 @@
+(function () {
+    const iconMenu = document.querySelector('.menu_icon');
+    const menuBody = document.querySelector('.menu');
+    const menuSocialIcons = document.querySelector('.social-networks-fa-burger');
+    const tel = document.querySelector('.phonecell');
+  
+    iconMenu.addEventListener('click', function (e) {
+      document.body.classList.toggle('_lock');
+      iconMenu.classList.toggle('_active');
+      menuBody.classList.toggle('_active');
+      menuSocialIcons.classList.toggle('_active');
+      tel.classList.toggle('_active');
+    });
+  
+    document
+      .querySelectorAll('.menu a')
+      .forEach((menuItem) => menuItem.addEventListener('click', hideMenu));
+  
+    function hideMenu() {
+      document.body.classList.remove('_lock');
+      iconMenu.classList.remove('_active');
+      menuBody.classList.remove('_active');
+      menuSocialIcons.classList.remove('_active');
+      tel.classList.remove('_active');
+    }
+  })();
+  
